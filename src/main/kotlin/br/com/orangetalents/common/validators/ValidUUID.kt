adaf9349a02d08553bea8ce9313a7a973d7eed63
Alexandre Szilagyi Santos <alexandre.szilagyi.santoz@gmail.com>
2021-07-16 22:04:39 -0400

    Corrigida falha crítica com o validador de UUID, que por alguma razão, ele consegue criar UUIDs que tenham apenas alguns poucos caracteres na primeira secção

--- src/main/kotlin/br/com/orangetalents/common/validators/ValidUUID.kt
+++ src/main/kotlin/br/com/orangetalents/common/validators/ValidUUID.kt
@@ -7,16 +7,23 @@ import java.util.*
 import javax.inject.Singleton
 import javax.validation.Constraint
 import javax.validation.Payload
+import javax.validation.ReportAsSingleViolation
+import javax.validation.constraints.Pattern
 import kotlin.annotation.AnnotationRetention.RUNTIME
 import kotlin.annotation.AnnotationTarget.*
 import kotlin.reflect.KClass
 
 @MustBeDocumented
+@Pattern(
+    regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
+    flags = [Pattern.Flag.CASE_INSENSITIVE]
+)
+@ReportAsSingleViolation
 @Constraint(validatedBy = [ValidUUIDValidator::class])
 @Retention(RUNTIME)
 @Target(FIELD, CONSTRUCTOR, PROPERTY, VALUE_PARAMETER)
 annotation class ValidUUID(
-    val message: String = "não é um formato válido de UUID",
+    val message: String = "formato inválido de identificador: (\${validatedValue)",
     val groups: Array<KClass<Any>> = [],
     val payload: Array<KClass<Payload>> = [],
 )
